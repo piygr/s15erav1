bd21f1d500f002dae5daa7273caeb09b487a4ac9
piygr <grover1piyush@gmail.com>
2023-09-01 17:04:16 +0530

    add/del files

--- main.py
+++ main.py
@@ -37,8 +37,3 @@ def init(train=True):
             cargs = dict(ckpt_path=config.get_weights_file_path(cfg, '1'))
 
         trainer.fit(model, train_dataloader, val_dataloader, **cargs)
-    '''else:
-        ckpt_file = kwargs.get('ckpt_file')
-        if ckpt_file:
-            checkpoint = utils.load_model_from_checkpoint(cfg.DEVICE, file_name=ckpt_file)
-            model.load_state_dict(checkpoint['model'], strict=False)'''
--- utils.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-def load_model_from_checkpoint(device, file_name='ckpt_light.pth'):
-    checkpoint = torch.load(file_name, map_location=device)
-
-    return checkpoint
